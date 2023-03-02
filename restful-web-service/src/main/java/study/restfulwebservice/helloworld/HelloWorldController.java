93e1d97e5d170af4db22f25f26231b9b1383c50b
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-03-02 17:15:24 +0900

    #3 - 코드 리팩토링
    파일 위치 변경

--- restful-web-service/src/main/java/study/restfulwebservice/helloworld/HelloWorldController.java
+++ restful-web-service/src/main/java/study/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package study.restfulwebservice;
+package study.restfulwebservice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
